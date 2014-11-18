f701bef702b68f06a2085bebcb8aab7a80e91d52
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-11-18 15:06:33 +0900

    app一覧取得。templateに追加

--- js/append.js
+++ js/append.js
@@ -4,45 +4,45 @@ $(function(){
   document.addEventListener('click',function(evt){
     var target = evt.target;
     if (target instanceof HTMLAnchorElement &&
-        target.href &&
-        target.href.indexOf('http') !== 0) {
-      evt.preventDefault();
-      chrome.tabs.getSelected(null,function(tab){
-        switch (evt.button) {
-          case LEFT_CLICK :
-            chrome.tabs.update(tab.id,{url:target.href});
-            break;
-          case CENTER_CLICK:
-            chrome.tabs.create({
-              url:target.href,
-              selected:false
+      target.href &&
+      target.href.indexOf('http') !== 0) {
+        evt.preventDefault();
+        chrome.tabs.getSelected(null,function(tab){
+          switch (evt.button) {
+            case LEFT_CLICK :
+              chrome.tabs.update(tab.id,{url:target.href});
+              break;
+              case CENTER_CLICK:
+                chrome.tabs.create({
+                  url:target.href,
+                  selected:false
+                });
+                break;
+              }
             });
-            break;
-        }
+          }
+        },false);
+
+        chrome.management.getAll(function(data){
+          console.log(data);
+          // debugger;
+          var app_temp = _.template($("#app").html());
+          for (var i = 0; i < data.length; i++) {
+            if(data[i].type == "package_app" || data[i].type == "hosted_app" || data[i].type == "legacy_packaged_app"){
+              console.log(data[i]);
+              var iconnum = data[i].icons.length - 1;
+              var app = {
+                "name" : data[i].name,
+                "url" : data[i].appLaunchUrl,
+                "icon_url" : data[i].icons[iconnum].url
+              };
+              debugger;
+               $("#app_list").append(app_temp(app));
+              console.log(app_temp(app));
+
+            }
+
+          }
+        });
+
       });
-    }
-  },false);
-
-  chrome.management.getAll(function(data){
-      console.log(data);
-      // debugger;
-      var app_temp = _.template($("#app").html());
-    for (var i = 0; i < data.length; i++) {
-      if(data[i].type == "package_app" || data[i].type == "hosted_app" || data[i].type == "legacy_packaged_app"){
-      console.log(data[i]);
-      var iconnum = data[i].icons.length - 1;
-      var app = {
-        "name" : data[i].name,
-        "url" : data[i].appLaunchUrl,
-        "icon_url" : data[i].icons[iconnum].url
-      };
-       $("#app_list").append("<p><a href='"   +data[i].appLaunchUrl + "'>" + "<img src='"+ data[i].icons[0].url + "width='32' height='' '>"  + "</a></p>");
-       $("#app_list").append();
-      console.log(data[i].appLaunchUrl);
-
-      }
-
-      }
-  });
-
-});
